e5517ab65c98cedeecc872cd7bd940daecd7e548
Ed Bueler <elbueler@alaska.edu>
2020-12-20 22:52:36 -0600

    Add option to generate a random obstacle.  (Cool.)

--- py/obs1.py
+++ py/obs1.py
@@ -34,9 +34,11 @@ parser.add_argument('-cycles', type=int, default=2, metavar='M',
                     help='number of V-cycles (default=2)')
 parser.add_argument('-downsweeps', type=int, default=1, metavar='N',
                     help='number of sweeps of projected Gauss-Seidel (default=1)')
+parser.add_argument('-fvalue', type=float, default=-2.0, metavar='X',
+                    help='in Poisson equation -u"=f this sets the constant f-value (default X=-2)')
 parser.add_argument('-jfine', type=int, default=2, metavar='J',
                     help='fine mesh is jth level (default jfine=2)')
-parser.add_argument('-lowobstacle', action='store_true', default=False,
+parser.add_argument('-low', action='store_true', default=False,
                     help='use obstacle sufficiently low to have no contact')
 parser.add_argument('-jcoarse', type=int, default=0, metavar='J',
                     help='coarse mesh is jth level (default jcoarse=0 gives 1 node)')
@@ -50,6 +52,10 @@ parser.add_argument('-o', metavar='FILE', type=str, default='',
                     help='save plot at end in image file, e.g. PDF or PNG')
 parser.add_argument('-pgs', action='store_true', default=False,
                     help='do projected Gauss-Seidel (instead of multigrid)')
+parser.add_argument('-random', action='store_true', default=False,
+                    help='use obstacle which is random perturbation of usual')
+parser.add_argument('-randommag', type=float, default=0.4, metavar='X',
+                    help='magnitude of -random perturbation (default X=0.4)')
 parser.add_argument('-show', action='store_true', default=False,
                     help='show plot at end')
 parser.add_argument('-symmetric', action='store_true', default=False,
@@ -61,42 +67,47 @@ if args.obs1help:
     parser.print_help()
     sys.exit(0)
 
-# FIXME add option for random-ish fine-mesh obstacle
-def phi(low,x):
+# fix the random seed for repeatability
+np.random.seed(1)
+
+def phi(x):
     '''The obstacle:  u >= phi.'''
-    if low:
-        return 8.0 * x * (1.0 - x) - 10.0
-    else:
-        return 8.0 * x * (1.0 - x) - 1.0
+    ph = 8.0 * x * (1.0 - x) - 1.0
+    if args.low:
+        ph -= 9.0
+    if args.random:
+        ph += args.randommag * np.random.rand(len(x))
+    return ph
 
 def fsource(x):
     '''The source term in -u'' = f.'''
-    return - 2.0 * np.ones(np.shape(x))
+    return args.fvalue * np.ones(np.shape(x))
 
 def fzero(x):
     return np.zeros(np.shape(x))
 
-def uexact(low,x):
+def uexact(x):
     '''A by-hand calculation from -u''=-2, and given the above obstacle, shows
     that u(a)=phi(a), u'(a)=phi'(a) for a<1/2 implies a=1/3.'''
-    if low:
+    if args.low:
         u = x * (x - 1.0)   # solution without obstruction
     else:
         a = 1.0/3.0
         def upoisson(x):
             return x * (x - 18.0 * a + 8.0)
-        u = phi(False,x)
+        u = phi(x)
         u[x < a] = upoisson(x[x < a])
         u[x > 1.0-a] = upoisson(1.0 - x[x > 1.0-a])
     return u
 
-def finalplot(xx,uinitial,ufinal):
+def finalplot(xx,uinitial,ufinal,phifine):
     plt.figure(figsize=(15.0,8.0))
     plt.plot(xx,uinitial,'k--',label='initial iterate',linewidth=2.0)
     plt.plot(xx,ufinal,'k',label='final iterate',linewidth=4.0)
-    plt.plot(xx,phi(args.lowobstacle,xx),'r',label='obstacle',linewidth=2.0)
-    plt.plot(xx,uexact(args.lowobstacle,xx),'g',label='exact',linewidth=2.0)
-    plt.axis([0.0,1.0,-0.3,1.1])
+    plt.plot(xx,phifine,'r',label='obstacle',linewidth=2.0)
+    if not args.random:
+        plt.plot(xx,uexact(xx),'g',label='exact',linewidth=2.0)
+    plt.axis([0.0,1.0,-0.3,1.1*max(phifine)])
     plt.legend(fontsize=20.0)
     plt.xlabel('x')
 
@@ -108,54 +119,50 @@ for k in range(args.jcoarse,args.jfine+1):
 mesh = hierarchy[-1]  # fine mesh
 
 # discrete obstacle on fine level
-phifine = phi(args.lowobstacle,mesh.xx())
+phifine = phi(mesh.xx())
 
 # feasible initial iterate
 uinitial = np.maximum(phifine,np.zeros(np.shape(mesh.xx())))
 
 # ability to evaluate error
 def l2err(u):
-    udiff = u - uexact(args.lowobstacle,mesh.xx())
+    udiff = u - uexact(mesh.xx())
     return mesh.l2norm(udiff)
 
 # monotone multigrid V-cycles (unless user just wants pGS)
+uu = uinitial.copy()
 if args.pgs:
     # sweeps of projected Gauss-Seidel on fine grid
-    uu = uinitial.copy()
     r = mesh.residual(mesh.zeros(),fsource)
     for s in range(args.downsweeps):
         pgssweep(mesh.m,mesh.h,uu,r,phifine)
         if args.symmetric:
             pgssweep(mesh.m,mesh.h,uu,r,phifine,backward=True)
 else:
-    if args.monitor:
-        print('  0:  |u-uexact|_2 = %.4e' % (l2err(uinitial)))
-    uu = vcycle(uinitial,phifine,fsource,hierarchy,
-                levels=levels,view=args.mgview,symmetric=args.symmetric,
-                downsweeps=args.downsweeps,
-                coarsesweeps=args.coarsesweeps,
-                upsweeps=args.upsweeps)
-    for s in range(args.cycles-1):
+    for s in range(args.cycles):
         if args.monitor:
-            print('  %d:  |u-uexact|_2 = %.4e' % (s+1,l2err(uu)))
+            print('  %d:  |u-uexact|_2 = %.4e' % (s,l2err(uu)))
         uu = vcycle(uu,phifine,fsource,hierarchy,
                     levels=levels,view=args.mgview,symmetric=args.symmetric,
                     downsweeps=args.downsweeps,
                     coarsesweeps=args.coarsesweeps,
                     upsweeps=args.upsweeps)
 
-# evaluate numerical error
+# report on computation including numerical error
 if args.pgs:
-    print('level %d (m = %d) using with %d sweeps of pGS:  |u-uexact|_2 = %.4e' \
-          % (args.jfine,mesh.m,args.downsweeps,l2err(uu)))
+   method = 'with %d sweeps of pGS' % args.downsweeps
 else:
-    print('level %d (m = %d) using %2d V(%d,%d,%d) cycles:  |u-uexact|_2 = %.4e' \
-          % (args.jfine,mesh.m,args.cycles,
-             args.downsweeps,args.coarsesweeps,args.upsweeps,l2err(uu)))
+   method = 'using %d V(%d,%d,%d) cycles' \
+            % (args.cycles,args.downsweeps,args.coarsesweeps,args.upsweeps)
+if args.random:
+   error = ''
+else:
+   error = ':  |u-uexact|_2 = %.4e' % l2err(uu)
+print('level %d (m = %d) %s%s' % (args.jfine,mesh.m,method,error))
 
 # graphical output if desired
 if args.show or args.o:
-    finalplot(mesh.xx(),uinitial,uu)
+    finalplot(mesh.xx(),uinitial,uu,phifine)
 
     # FIXME put option control on this figure which shows defect hierarchy
     if False:
