bf23e2f1198f9c6996c1887d29ccb39039b8ac09
Ed Bueler <elbueler@alaska.edu>
2021-02-19 17:28:39 -0900

    Simplify to equally-spaced, m=8 fine mesh.

--- paper/genfigs/hats1d.py
+++ paper/genfigs/hats1d.py
@@ -21,8 +21,6 @@ def finemeshaxes(m):
     x = np.linspace(0.0,1.0,2)
     y = np.linspace(0.0,1.0,2)
     xdots = np.linspace(0.0,1.0,m+1)
-    np.random.seed(1)
-    xdots[1:m] += 0.012 * np.random.randn(m-1)
     plt.plot(x, np.zeros(2), 'k', lw=1.0)
     plt.plot(np.zeros(2), y, 'k', lw=1.0)
     return xdots
@@ -32,26 +30,27 @@ def hats(xdots,style='k',width=2.0,dotstyle='k.',dotsize=18.0):
         plt.plot(xdots[k:k+3],[0.0,1.0,0.0],style,lw=width)
     plt.plot(xdots[1:-1], np.zeros(len(xdots)-2),dotstyle,ms=dotsize)
 
-m = 12
+m = 8
 
 # fine hats figure
 plt.figure(figsize=(10,4))
 xdots = finemeshaxes(m)
+h = xdots[1] - xdots[0]
 plt.text(-0.01,-0.2,'0',fontsize=fsize)
 plt.text(0.99,-0.2,'1',fontsize=fsize)
 hats(xdots,style='k:',width=1.0,dotsize=14.0)
-plt.plot(xdots[8:11],[0.0,1.0,0.0],'k',lw=3.0)
-plt.text(xdots[9]-0.02,-0.2,r'$x_p$',fontsize=bigfsize)
+plt.plot(xdots[m-4:m-1],[0.0,1.0,0.0],'k',lw=3.0)
+plt.text(xdots[m-3]-0.02,-0.2,r'$x_p$',fontsize=bigfsize)
 plt.annotate(r'$p=1$',
-             xy=(xdots[1],0.02), xytext=(xdots[0]+0.025,0.25),
+             xy=(xdots[1],0.02), xytext=(xdots[0]+0.05,0.25),
              arrowprops=dict(arrowstyle='-|>',facecolor='black'),
              fontsize=fsize)
 plt.annotate(r'$p=m$',
-             xy=(xdots[m-1],0.02), xytext=(xdots[m],0.25),
+             xy=(xdots[m-1],0.02), xytext=(xdots[m]-0.02,0.35),
              arrowprops=dict(arrowstyle='-|>',facecolor='black'),
              fontsize=fsize)
 plt.annotate(r'$\psi_p(x)$',
-             xy=(xdots[9]+0.025, 0.6), xytext=(xdots[12], 0.8),
+             xy=(xdots[m-2]-0.08, 0.6), xytext=(xdots[m]-0.05, 0.8),
              arrowprops=dict(arrowstyle='-|>',facecolor='black'),
              fontsize=bigfsize)
 plt.text(-0.05,0.0,'0',fontsize=fsize)
