bd6d2708833532a743b0eda0056c69b42727b844
Ed Bueler <elbueler@alaska.edu>
2021-03-07 22:19:40 -0900

    Plot some performance results.

--- paper/genfigs/performance.py
+++ paper/genfigs/performance.py
@@ -3,16 +3,16 @@
 # see py/1D/study/convperf.sh  to generate input data performance.txt
 # FIXME only consider case = icelike+random (as more representative)
 # FIXME process into two performance figures each showing all four solvers
-#    Figure:  semilogx with N on x-axis and WU on y axis
-#    Figure:  loglog with N on x-axis and time on y axis;  result is time = O(N^xx)
+#    Figure:  semilogx with m on x-axis and WU on y axis
+#    Figure:  loglog with m on x-axis and time on y axis;  result is time = O(m^xx)
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 INFILE = 'performance.txt'
-SOLVENAME = ['V(1,0)', 'V(2,0)', 'NI+V(1,0)', 'NIx2+V(1,0)']
-MARKER = ['o','s','*','+']
-MARKERSIZE = [12.0,10.0,12.0,12.0]
+SOLVERNAME = ['V(1,0)', 'V(2,0)', 'NI+V(1,0)', 'NIx2+V(1,0)']
+MARKER = ['o','s','*','p']
+MARKERSIZE = [12.0,10.0,12.0,10.0]
 MARKERFACE = ['w','w','k','k']
 
 SHOW = False
@@ -31,28 +31,53 @@ assert len(v[:,0]) == 8*N   # 8 solver+problem combinations
 
 # columns:  solver case J m cycles WU time
 case = v[:,1]
+solver = v[case==1,0]
 m = v[case==1,3]
 h = 1.0 / (m+1)
 cycles = v[case==1,4]
 WU = v[case==1,5]
 time = v[case==1,6]
 
-# numerical error versus h
+# work units versus m
 plt.figure(figsize=(7,6))
-for p in range(2):
-    hh = h[prob==p]
-    ee = err[prob==p]
-    q = np.polyfit(np.log(hh),np.log(ee),1)
-    print('%10s: O(h^%.2f)' % (PROBLEMNAME[p],q[0]))
-    plt.loglog(hh,ee,
-               'k'+MARKER[p],ms=MARKERSIZE[p],mfc=MARKERFACE[p],
-               label=PROBLEMNAME[p] + ' $O(h^{%.2f})$' % q[0])
-    plt.loglog(hh,np.exp(q[0]*np.log(hh) + q[1]),'k--',lw=0.5)
+for s in range(4):  # s=solver
+    mm = m[solver==s]
+    WUU = WU[solver==s]
+    q = np.polyfit(np.log(mm),np.log(WUU),1)
+    print('%15s: WU = O(m^%.2f)' % (SOLVERNAME[s],q[0]))
+    plt.semilogx(mm,WUU,
+                 'k'+MARKER[s],ms=MARKERSIZE[s],mfc=MARKERFACE[s],
+                 label=SOLVERNAME[s] + ':  $O(m^{%.2f})$' % q[0])
 plt.grid(True)
-plt.xlabel('h',fontsize=18.0)
-plt.ylabel(r'$\|u^h-u\|_2$',fontsize=18.0)
+plt.xlabel('$m$  (degrees of freedom)',fontsize=18.0)
+plt.ylabel('WU',fontsize=18.0)
+plt.xlim([100.0,1.0e5])
+plt.ylim([0.0,100.0])
 plt.xticks(fontsize=14.0)
 plt.yticks(fontsize=14.0)
 plt.minorticks_off()
 plt.legend(loc='lower right',fontsize=14.0)
-writeout('convergence.pdf')
+writeout('mcdl-wu.pdf')
+
+# (time/m) versus m
+plt.figure(figsize=(7,6))
+for s in range(4):  # s=solver
+    timeper = time[solver==s] / mm
+    mmhigh = mm[mm > 4000]
+    timeperhigh = timeper[mm > 4000]
+    q = np.polyfit(np.log(mmhigh),np.log(timeperhigh),1)
+    print('%15s: time/m = O(m^%.2f)' % (SOLVERNAME[s],q[0]))
+    plt.semilogx(mm,timeper*1000.0,
+               'k'+MARKER[s],ms=MARKERSIZE[s],mfc=MARKERFACE[s],
+               label=SOLVERNAME[s] + ':  $O(m^{%.2f})$' % q[0])
+    plt.semilogx(mmhigh,1000.0*np.exp(q[0]*np.log(mmhigh) + q[1]),'k--',lw=0.5)
+plt.grid(True)
+plt.xlabel('$m$  (degrees of freedom)',fontsize=18.0)
+plt.ylabel('time (ms) per $m$',fontsize=18.0)
+plt.xlim([100.0,1.0e5])
+plt.ylim([0.0,4.5])
+plt.xticks(fontsize=14.0)
+plt.yticks(fontsize=14.0)
+plt.minorticks_off()
+plt.legend(loc='upper right',fontsize=14.0)
+writeout('mcdl-timeper.pdf')
