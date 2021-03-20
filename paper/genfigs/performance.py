7835ef597d2629af2c8d0ed5028cc8ed70a4c134
Ed Bueler <elbueler@alaska.edu>
2021-03-19 17:24:22 -0800

    Now performance.[sh|py] generates figures for V cycles only (not nested it).

--- paper/genfigs/performance.py
+++ paper/genfigs/performance.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
-# see py/1D/study/convperf.sh  to generate input data performance.txt
-# here we only consider icelike+random case (as more representative)
-# processed into two performance figures each showing all four solvers
-#    Figure:  semilogx with m on x-axis and number of slash cycles on y axis
+# see py/1D/study/performance.sh  to generate input data performance.txt
+# here we only consider icelike case
+
+# processed into three performance figures each showing four solvers
+#    Figure:  semilogx with m on x-axis and number of cycles on y axis
 #    Figure:  semilogx with m on x-axis and WU on y axis
-#    Figure:  loglog with m on x-axis and time on y axis; result is time = O(m^xx)
+#    Figure:  loglog with m on x-axis and time on y axis
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 INFILE = 'performance.txt'
-SOLVERNAME = ['V(1,0)', 'V(2,0)', 'F+V(1,0)', 'Fx2+V(1,0)']
-MARKER = ['o','s','*','p']
-MARKERSIZE = [12.0,10.0,12.0,10.0]
+SOLVERNAME = ['V(1,0)', 'V(0,1)', 'V(1,1)', 'V(1,1)-J']
+MARKER = ['o','s','v','D']
+MARKERSIZE = [12.0,11.0,10.0,9.0]
 MARKERFACE = ['w','w','k','k']
 
 SHOW = False
@@ -28,17 +29,15 @@ print('reading %s ...' % INFILE)
 v = np.array(np.loadtxt(INFILE))
 
 N = 10  # number of different resolutions
-assert len(v[:,0]) == 8*N   # 8 solver+problem combinations
+assert len(v[:,0]) == 4*N   # 8 solver+problem combinations
 
-# columns:  solver case J m cycles WU time
-case = v[:,1]
-# only consider -random cases
-solver = v[case==1,0]
-m = v[case==1,3]
+# columns:  solver J m cycles WU time
+solver = v[:,0]
+m = v[:,2]
 h = 1.0 / (m+1)
-cycles = v[case==1,4]
-WU = v[case==1,5]
-time = v[case==1,6]
+cycles = v[:,3]
+WU = v[:,4]
+time = v[:,5]
 
 # cycles versus m
 plt.figure(figsize=(7,6))
@@ -48,11 +47,11 @@ for s in range(4):  # s=solver
     q = np.polyfit(np.log(mm),np.log(ccycles),1)
     print('%15s: cycles = O(m^%.2f)' % (SOLVERNAME[s],q[0]))
     plt.semilogx(mm,ccycles,'k'+MARKER[s],ms=MARKERSIZE[s],mfc=MARKERFACE[s],
-                 label=SOLVERNAME[s])
+                 label=SOLVERNAME[s] + ':  $O(m^{%.2f})$' % q[0])
 plt.grid(True)
-plt.xlabel('$m$  (degrees of freedom)',fontsize=18.0)
-plt.ylabel('cycles',fontsize=18.0)
-plt.axis([100.0,1.0e5,0.0,41.0])
+plt.xlabel('$m$',fontsize=18.0)
+plt.ylabel('iterations',fontsize=18.0)
+plt.axis([100.0,1.0e5,0.0,55.0])
 plt.xticks(fontsize=14.0)
 plt.yticks(fontsize=14.0)
 plt.minorticks_off()
@@ -67,36 +66,38 @@ for s in range(4):  # s=solver
     q = np.polyfit(np.log(mm),np.log(WUU),1)
     print('%15s: WU = O(m^%.2f)' % (SOLVERNAME[s],q[0]))
     plt.semilogx(mm,WUU,'k'+MARKER[s],ms=MARKERSIZE[s],mfc=MARKERFACE[s],
-                 label=SOLVERNAME[s])
+                 label=SOLVERNAME[s] + ':  $O(m^{%.2f})$' % q[0])
 plt.grid(True)
-plt.xlabel('$m$  (degrees of freedom)',fontsize=18.0)
+plt.xlabel('$m$',fontsize=18.0)
 plt.ylabel('WU',fontsize=18.0)
-plt.axis([100.0,1.0e5,0.0,100.0])
+plt.axis([100.0,1.0e5,0.0,210.0])
 plt.xticks(fontsize=14.0)
 plt.yticks(fontsize=14.0)
 plt.minorticks_off()
-plt.legend(loc='lower right',fontsize=14.0)
+plt.legend(loc='upper left',fontsize=14.0)
 writeout('mcdl-wu.pdf')
 
-# (time/m) versus m
+# (time/m) versus m; skip three coarsest
 plt.figure(figsize=(7,6))
 for s in range(4):  # s=solver
+    mm = m[solver==s]
     timeper = time[solver==s] / mm
+    mmall = mm[mm > 1000]
+    timeperall = timeper[mm > 1000]
     mmhigh = mm[mm > 4000]
     timeperhigh = timeper[mm > 4000]
     q = np.polyfit(np.log(mmhigh),np.log(timeperhigh),1)
     print('%15s: time/m = O(m^%.2f)' % (SOLVERNAME[s],q[0]))
-    plt.semilogx(mm,timeper*1000.0,
+    plt.semilogx(mmall,timeperall*1000.0,
                'k'+MARKER[s],ms=MARKERSIZE[s],mfc=MARKERFACE[s],
                label=SOLVERNAME[s] + ':  $O(m^{%.2f})$' % q[0])
     plt.semilogx(mmhigh,1000.0*np.exp(q[0]*np.log(mmhigh) + q[1]),'k--',lw=0.5)
 plt.grid(True)
-plt.xlabel('$m$  (degrees of freedom)',fontsize=18.0)
+plt.xlabel('$m$',fontsize=18.0)
 plt.ylabel('time (ms) per $m$',fontsize=18.0)
-plt.xlim([100.0,1.0e5])
-plt.ylim([0.0,4.5])
+plt.axis([900.0,1.0e5,0.0,1.5])
 plt.xticks(fontsize=14.0)
 plt.yticks(fontsize=14.0)
 plt.minorticks_off()
-plt.legend(loc='upper right',fontsize=14.0)
+plt.legend(loc='lower right',fontsize=14.0)
 writeout('mcdl-timeper.pdf')
--- py/1D/study/performance.sh
+++ py/1D/study/performance.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
-# performance (WU and timing) for the classical obstacle problem with:
-#      V(1,0), V(0,1), V(1,1), ni+V(0,1)
-#   on default problem icelike using default rtol=1e-3
+# performance (WU and timing) for V-cycles of the classical obstacle problem
+# four cases:
+#      V(1,0), V(0,1), V(1,1), V(1,1)-Jacobi0.67
+# on default problem icelike using default rtol=1e-3
 
 # to save results:
 #   $ ./performance.sh >& out.performance
 
-for OPTS in "-down 1 -up 0" "-down 0 -up 1" "-down 1 -up 1" "-ni -down 0 -up 1"; do
+for OPTS in "-down 1 -up 0" "-down 0 -up 1" "-down 1 -up 1" "-down 1 -up 1 -jacobi -omega 0.67"; do
     echo "*** solver = '$OPTS' ****"
     for JJ in 6 7 8 9 10 11 12 13 14 15; do
+    #for JJ in 6 7 8 9 10 11 12; do
         /usr/bin/time -f "real %e" python3 ../obstacle.py -poissoncase icelike -jfine $JJ $OPTS
     done
 done
