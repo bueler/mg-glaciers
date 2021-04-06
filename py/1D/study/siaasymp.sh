98708aeddc5781b286aa30c8b3d7f631d306eaa2
Ed Bueler <elbueler@alaska.edu>
2021-04-05 21:39:31 -0800

    Set up some scripts to start measuring SIA performance.

---
+++ py/1D/study/siaasymp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# measure asymptotic rates around exact solution for V-cycles with various smoothers
+# to save results:
+#   $ ./siaasymp.sh >& out.siaasymp
+
+# quick view results:
+#   $ grep -B 2 case out.siaasymp |less
+
+#for SMOOTHER in "" "-jacobi -omega 0.5"; do
+for SMOOTHER in ""; do
+    for CYCLE in "" "-down 1" "-symmetric" "-symmetric -down 1" "-symmetric -down 1 -up 2"; do
+        for JJ in 6 8 10 12; do
+            OPTS="-problem sia -exactinitial -monitor -irtol 1.0e-8 -J $JJ $SMOOTHER $CYCLE"
+            echo "case: " $OPTS
+            python3 ../obstacle.py $OPTS
+        done
+    done
+done
