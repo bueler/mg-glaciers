adac77b1545561f0f2175b608f25b3ea6447d986
Ed Bueler <elbueler@alaska.edu>
2021-04-07 08:01:25 -0800

    Study some omega values for SIA Jacobi.

--- py/1D/study/siaasymp.sh
+++ py/1D/study/siaasymp.sh
@@ -10,10 +10,8 @@ set -e
 
 # OTHER PARAMETERS TO CONSIDER:
 #   -newtonits 1|2|3
-#   -jacobi -omega 0.2|0.3|0.4|0.5|0.6
 
-#for SMOOTHER in "" "-jacobi -omega 0.5"; do
-for SMOOTHER in ""; do
+for SMOOTHER in "" "-jacobi -omega 0.6" "-jacobi -omega 0.5" "-jacobi -omega 0.4" "-jacobi -omega 0.3" "-jacobi -omega 0.2"; do
     for CYCLE in "" "-down 1" "-symmetric" "-symmetric -down 1" "-symmetric -down 1 -up 2"; do
         for JJ in 6 8 10 12; do
             OPTS="-problem sia -exactinitial -monitor -irtol 1.0e-8 -J $JJ $SMOOTHER $CYCLE"
