cef6e013cf9af3b8c7adeed226978b79a386e510
Ed Bueler <elbueler@alaska.edu>
2021-04-11 11:30:23 -0800

    Update convergence.sh script so it generates same errors as before.

--- py/1D/study/convergence.sh
+++ py/1D/study/convergence.sh
@@ -6,11 +6,11 @@ set -e
 # to save results:
 #   $ ./convergence.sh >& out.convergence
 
-# convergence runs with V(1,0) and tight tolerance
+# convergence runs with V(0,2) and tight tolerance
 for CASE in icelike traditional pde2; do
     echo "*** case $CASE ****"
     for JJ in 2 3 4 5 6 7 8 9 10 11 12 13 14; do
-        python3 ../obstacle.py -poissoncase $CASE -J $JJ -irtol 1.0e-7 -cyclemax 1000
+        python3 ../obstacle.py -poissoncase $CASE -J $JJ -irtol 1.0e-8 -cyclemax 1000
     done
 done
 echo
