bb6c28498f4335d7cc2c1bf35e6fe2ea7b4e0311
Ed Bueler <elbueler@alaska.edu>
2021-04-11 13:00:53 -0800

    Simplify asymptotic rates SIA runs.

--- py/1D/study/siaasymp.sh
+++ py/1D/study/siaasymp.sh
@@ -6,17 +6,15 @@ set -e
 #   $ ./siaasymp.sh >& out.siaasymp
 
 # quick view results:
-#   $ grep -B 2 case out.siaasymp |less
+#   $ grep -B 4 case out.siaasymp |less
 
 # OTHER PARAMETERS TO CONSIDER:
 #   -newtonits 1|2|3
 
-for SMOOTHER in "" "-jacobi -omega 0.6" "-jacobi -omega 0.5" "-jacobi -omega 0.4" "-jacobi -omega 0.3" "-jacobi -omega 0.2"; do
-    for CYCLE in "" "-down 1" "-symmetric" "-symmetric -down 1" "-symmetric -down 1 -up 2"; do
-        for JJ in 6 8 10 12; do
-            OPTS="-problem sia -exactinitial -monitor -irtol 1.0e-8 -J $JJ $SMOOTHER $CYCLE"
-            echo "case: " $OPTS
-            python3 ../obstacle.py $OPTS
-        done
+for SMOOTHER in "" "-down 1" "-down 2" "-jacobi -omega 0.6" "-jacobi -omega 0.5" "-jacobi -omega 0.4" "-jacobi -omega 0.3"; do
+    for JJ in 6 8 10; do
+        OPTS="-problem sia -exactinitial -monitor -irtol 1.0e-8 -J $JJ $SMOOTHER"
+        echo "case: " $OPTS
+        python3 ../obstacle.py $OPTS
     done
 done
