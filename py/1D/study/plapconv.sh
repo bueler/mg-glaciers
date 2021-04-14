34e97ba5f26cb4dfb8171bfbc77aa6a50958dfa8
Ed Bueler <elbueler@alaska.edu>
2021-04-13 16:24:38 -0800

    Missed in previous commit (4277a8dca8).

--- py/1D/study/plapconv.sh
+++ py/1D/study/plapconv.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-# measure convergence for p-Laplacian obstacle problem
-# compare siaconv.sh
+# measure convergence for p-Laplacian pile obstacle problem
 
 # to save results:
 #   $ ./plapconv.sh >& out.plapconv
 
 # convergence runs with F-cycle V(1,2) and tight tolerance
 for JJ in 2 3 4 5 6 7 8 9 10 11 12 13 14; do
-    python3 ../obstacle.py -problem plap -down 1 -ni -nicycles 3 -J $JJ -irtol 1.0e-10 -cyclemax 1000
+    python3 ../obstacle.py -problem plap -plapcase pile -down 1 -ni -nicycles 3 -J $JJ -irtol 1.0e-8 -cyclemax 1000
 done
