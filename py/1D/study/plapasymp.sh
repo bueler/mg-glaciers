3146e689aa68daad19b0bc87014b705e3d33f3f4
Ed Bueler <elbueler@alaska.edu>
2021-04-13 16:36:42 -0800

    Draft scripts for measuring asymptotic rates.

--- py/1D/study/plapasymp.sh
+++ py/1D/study/plapasymp.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -e
 
-# for SIA problem, measure asymptotic rates around exact solution
+# for p-Laplacian pile problem, measure asymptotic rates around exact solution
 # for V-cycles with various smoothers
 
 # to save results:
-#   $ ./siaasymp.sh >& out.siaasymp
+#   $ ./plapasymp.sh >& out.plapasymp
 
 # quick view results:
-#   $ grep -B 4 cycles out.siaasymp |less
+#   $ grep -B 4 cycles out.plapasymp |less
 
 # OTHER PARAMETERS TO CONSIDER:
 #   -newtonits 1|2|3
 
 for SMOOTHER in "" "-down 1" "-down 2" "-jacobi -omega 0.6" "-jacobi -omega 0.5" "-jacobi -omega 0.4"; do
     for JJ in 6 8 10; do
-        OPTS="-problem sia -exactinitial -monitor -irtol 1.0e-8 -J $JJ $SMOOTHER"
+        OPTS="-problem plap -plapcase pile -exactinitial -monitor -irtol 1.0e-8 -J $JJ $SMOOTHER"
         echo "case: " $OPTS
         python3 ../obstacle.py $OPTS
     done
