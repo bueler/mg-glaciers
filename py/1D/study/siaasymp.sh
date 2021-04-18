d3a999bd8f30aef2841047b0c612f5ef5d4729fd
Ed Bueler <elbueler@alaska.edu>
2021-04-18 13:34:50 -0800

    Comments describe experiments with measuring asymptotic rates.  (Need to fix the excursions first ...)

--- py/1D/study/siaasymp.sh
+++ py/1D/study/siaasymp.sh
@@ -13,11 +13,14 @@ set -e
 # OTHER PARAMETERS TO CONSIDER:
 #   -newtonits 1|2|3
 
-PROBLEM=sia  # change to plap to compare
+PROBLEM="-problem sia -siacase profile -exactinitial"  # out.siaasymp
+#PROBLEM="-problem plap -plapcase pile -exactinitial"  # out.plapasymp
+#PROBLEM="-problem sia -siacase bumpy -ni -nicycles 10"  # out.bumpy.siaasymp
+#PROBLEM="-problem sia -siacase bumpy -ni -nicycles 10 -siaeps 500.0"  # out.bumpy.eps500.siaasymp
 
 for SMOOTHER in "" "-down 1" "-down 2" "-jacobi -omega 0.6" "-jacobi -omega 0.5" "-jacobi -omega 0.4"; do
     for JJ in 4 6 8 10; do
-        OPTS="-problem $PROBLEM -exactinitial -monitor -irtol 1.0e-8 -J $JJ $SMOOTHER"
+        OPTS="$PROBLEM -monitor -irtol 1.0e-8 -J $JJ $SMOOTHER"
         echo "case: " $OPTS
         python3 ../obstacle.py $OPTS
     done
