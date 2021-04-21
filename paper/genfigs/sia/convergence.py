7f18517327d81510196b2cea161ea823f0c00996
Ed Bueler <elbueler@alaska.edu>
2021-04-20 16:20:34 -0800

    Add L^1 error to SIA convergence figure.

--- paper/genfigs/sia/convergence.py
+++ paper/genfigs/sia/convergence.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 from writeout import writeout
 
 INFILE = 'convergence.txt'
-MARKER = ['o','o']
-MARKERSIZE = [10.0,10.0]
-MARKERFACE = ['w','k']
-NAME = ['$|s-s_{exact}|_2$','$|H^r-H_{exact}^r|_p$']
+MARKER = ['o','s','D']
+MARKERSIZE = [10.0,9.0,8.0]
+MARKERFACE = ['w','w','k']
+NAME = ['$|s-s_{exact}|_1$','$|s-s_{exact}|_2$','$|H^r-H_{exact}^r|_p$']
 
 print('reading %s ...' % INFILE)
 v = np.array(np.loadtxt(INFILE))
@@ -24,25 +24,31 @@ L = 1800.0e3  # m
 m = v[:,1]
 h = L / (m+1)
 h /= 1000.0   # show in km
-err2 = v[:,2]
-errrp = v[:,3]
+err1 = v[:,2]
+err2 = v[:,3]
+errrp = v[:,4]
 
 # numerical error versus h
 plt.figure(figsize=(7,6))
+q1 = np.polyfit(np.log(h),np.log(err1),1)
 q2 = np.polyfit(np.log(h),np.log(err2),1)
 qrp = np.polyfit(np.log(h),np.log(errrp),1)
-print('|s-sexact|_2 = O(h^%.2f), |H^r - Hexact^r|_p = O(h^%.2f)' \
-      % (q2[0],qrp[0]))
-plt.loglog(h,err2,
+print('|s-sexact|_1 = O(h^%.2f), |s-sexact|_2 = O(h^%.2f), |H^r - Hexact^r|_p = O(h^%.2f)' \
+      % (q1[0],q2[0],qrp[0]))
+plt.loglog(h,err1,
            'k'+MARKER[0],ms=MARKERSIZE[0],mfc=MARKERFACE[0],
-           label=NAME[0] + '$=O(h^{%.2f})$' % q2[0])
+           label=NAME[0] + '$=O(h^{%.2f})$' % q1[0])
+plt.loglog(h,np.exp(q1[0]*np.log(h)+q1[1]),'k--')
+plt.loglog(h,err2,
+           'k'+MARKER[1],ms=MARKERSIZE[1],mfc=MARKERFACE[1],
+           label=NAME[1] + '$=O(h^{%.2f})$' % q2[0])
 plt.loglog(h,np.exp(q2[0]*np.log(h)+q2[1]),'k--')
 plt.loglog(h,errrp,
-           'k'+MARKER[1],ms=MARKERSIZE[1],mfc=MARKERFACE[1],
-           label=NAME[1] + '$=O(h^{%.2f})$' % qrp[0])
+           'k'+MARKER[2],ms=MARKERSIZE[2],mfc=MARKERFACE[2],
+           label=NAME[2] + '$=O(h^{%.2f})$' % qrp[0])
 plt.loglog(h,np.exp(qrp[0]*np.log(h)+qrp[1]),'k--')
 plt.grid(True)
-plt.axis([1.0e-2,1.0e3,1.0e2,1.0e11])
+plt.axis([1.0e-2,1.0e3,1.0e2,1.0e12])
 plt.xlabel('$h$  (km)',fontsize=18.0)
 plt.ylabel('numerical error',fontsize=18.0)
 plt.xticks(fontsize=14.0)
--- py/1D/study/siaconv.sh
+++ py/1D/study/siaconv.sh
@@ -8,5 +8,5 @@ set -e
 
 # convergence runs with F-cycle V(1,2) and tight tolerance
 for JJ in 2 3 4 5 6 7 8 9 10 11 12 13 14; do
-    python3 ../obstacle.py -problem sia -down 1 -ni -nicycles 3 -J $JJ -irtol 1.0e-10 -cyclemax 1000
+    python3 ../obstacle.py -problem sia -down 1 -ni -nicycles 3 -J $JJ -irtol 1.0e-10 -cyclemax 1000 -l1err
 done
